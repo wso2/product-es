ff740fe46e222fa7b4486aa03ce5668ae580c5c0
sameeram <mgjaye@gmail.com>
2014-02-20 17:10:06 +0530

    Added tag-plugin to handle tags

--- modules/apps/publisher/themes/default/js/common/form-plugins/tag-plugin.js
+++ modules/apps/publisher/themes/default/js/common/form-plugins/tag-plugin.js
@@ -1,58 +1,61 @@
-$(function(){
+$(function () {
 
-    var DEFAULT_TAG_THEME='facebook';
-    var ALLOW_FREE_TAG=false;
+    var DEFAULT_TAG_THEME = 'facebook';
+    var ALLOW_FREE_TAG = true;
 
 
-    function TagPlugin(){
-       this.tagContainer='';
+    function TagPlugin() {
+        this.tagContainer = '';
+        this.tagUrl = '';
     }
 
-    TagPlugin.prototype.init=function(element){
-       var tagUrl=element.meta.tagApi;
-       this.tagContainer='#'+element.meta.tagContainer||element.id;
-       if(!tagUrl){
-           console.log('Unable to locate tag api url');
-           return;
-       }
-       fetchInitTags(tagUrl,tagContainer);
+    TagPlugin.prototype.init = function (element) {
+        //TODO: Replace where we get the asset type from
+        var type = $('#meta-asset-type').val();
+        this.tagUrl = element.meta.tagApi+type;
+        this.tagContainer = '#' +element.id;
+        if (!this.tagUrl) {
+            console.log('Unable to locate tag api url');
+            return;
+        }
+        fetchInitTags(this.tagUrl, this.tagContainer);
     };
 
-    TagPlugin.prototype.getData=function(element){
-        var data={};
-        var tags;
+    TagPlugin.prototype.getData = function (element) {
+        var data = {};
+        var tags=[];
         var selectedTags;
 
-        selectedTags=$(this.tagContainer).tokenInput('get');
+        selectedTags = $(this.tagContainer).tokenInput('get');
 
-        for(var index in selectedTags){
+        for (var index in selectedTags) {
             tags.push(selectedTags[index].name);
         }
 
-        data[element.id]=JSON.stringify(tags);
+        data[element.id] = tags;
 
         return data;
     }
 
-    var fetchInitTags=function(tagUrl,tagContainer){
+    var fetchInitTags = function (tagUrl, tagContainer) {
 
         //Obtain all of the tags for the given asset type
         $.ajax({
-            url : tagUrl,
-            type : 'GET',
-            success : function(response) {
+            url: tagUrl,
+            type: 'GET',
+            success: function (response) {
                 var tags = JSON.parse(response);
                 $(tagContainer).tokenInput(tags, {
-                    theme : DEFAULT_TAG_THEME,
-                    allowFreeTagging :ALLOW_FREE_TAG
+                    theme: DEFAULT_TAG_THEME,
+                    allowFreeTagging: ALLOW_FREE_TAG
                 });
 
             },
-            error : function() {
+            error: function () {
                 console.log('unable to fetch tag cloud for ' + type);
             }
         });
     }
 
-    FormManager.register('TagPlugin',TagPlugin);
+    FormManager.register('TagPlugin', TagPlugin);
 });
