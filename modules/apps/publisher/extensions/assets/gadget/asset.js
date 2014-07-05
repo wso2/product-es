e71cc548e69a3534dab45293c73b9404f7c9a130
sameerahj <sameera.jayaratna@gmail.com>
2014-07-05 08:15:24 +0530

    loading pages from extensions

---
+++ modules/apps/publisher/extensions/assets/gadget/asset.js
@@ -0,0 +1,13 @@
+var assetLinks = function (user) {
+    return {
+        title: 'Gadget',
+        links: [
+            {
+                title: 'Bookmarks',
+                url: 'bookmarks',
+                path: 'bookmarks.jag'
+            }
+        ],
+        isCategorySupport: true
+    };
+};
