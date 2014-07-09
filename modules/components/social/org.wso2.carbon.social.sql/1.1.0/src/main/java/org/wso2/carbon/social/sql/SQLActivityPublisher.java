3ec9c82d99a474e9ebedf3691637bb4a5d3d172b
udarakr <udarakr@gmail.com>
2014-07-09 15:47:46 +0530

    updating SQLActivityPublisher

--- modules/components/social/org.wso2.carbon.social.sql/1.1.0/src/main/java/org/wso2/carbon/social/sql/SQLActivityPublisher.java
+++ modules/components/social/org.wso2.carbon.social.sql/1.1.0/src/main/java/org/wso2/carbon/social/sql/SQLActivityPublisher.java
@@ -1,21 +1,19 @@
 package org.wso2.carbon.social.sql;
 
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Connection;
-
-import javax.sql.DataSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mozilla.javascript.NativeObject;
+import org.wso2.carbon.social.sql.Constants;
 import org.wso2.carbon.social.core.ActivityPublisher;
 import org.wso2.carbon.social.core.JSONUtil;
 
 public class SQLActivityPublisher extends ActivityPublisher {
 	
 	private static Log log = LogFactory.getLog(SQLActivityPublisher.class);
-	public static final String INSERT_SQL = "INSERT INTO ES_SOCIAL(id, contextId, body) VALUES(?, ?, ?)";
+	public static final String INSERT_SQL = "INSERT INTO ES_SOCIAL(id, payload_context_id, body) VALUES(?, ?, ?)";
     @Override
     protected String publish(String id, NativeObject activity) {
     	DSConnection con = new DSConnection();
@@ -23,15 +21,24 @@ public class SQLActivityPublisher extends ActivityPublisher {
     	if(connection != null){
     		PreparedStatement statement = null;
             try{
-            String json = JSONUtil.SimpleNativeObjectToJson(activity);
-            String contextId = JSONUtil.getNullableProperty(activity, Constants.CONTEXT_JSON_PROP, Constants.ID_JSON_PROP);
-
-            statement = connection.prepareStatement(INSERT_SQL);
-            statement.setString(1, id);
-            statement.setString(2, contextId);
-            statement.setString(3, json);
-            statement.executeQuery();
-            }catch(Exception e){
+	            String json = JSONUtil.SimpleNativeObjectToJson(activity);
+	            String contextId = JSONUtil.getNullableProperty(activity, Constants.CONTEXT_JSON_PROP, Constants.ID_JSON_PROP);
+	            if (contextId == null) {
+	                contextId = JSONUtil.getProperty(activity, Constants.TARGET_JSON_PROP, Constants.ID_JSON_PROP);
+	            }
+	
+	            statement = connection.prepareStatement(INSERT_SQL);
+	            statement.setString(1, id);
+	            statement.setString(2, contextId);
+	            statement.setString(3, json);
+	            int ret = statement.executeUpdate();
+	            connection.commit();
+	            if(ret > 0){
+	            	return id;
+	            }else{
+	            	log.info("failed to publish social event.");
+	            }
+	            }catch(Exception e){
             	log.error("failed to publish social event.", e);
             }finally{
             	con.closeConnection(connection);
