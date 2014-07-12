19de01bea7a8be09a6834d1a27b2973479902f3e
sameeram <mgjaye@gmail.com>
2014-07-12 17:26:07 +0530

    Added  asset listing

--- modules/apps/publisher/themes/default/renderers/pages/list.js
+++ modules/apps/publisher/themes/default/renderers/pages/list.js
@@ -1,11 +1,13 @@
-var render=function(theme,data,meta,require){
-	theme('single-col-fluid',{
+var render = function(theme, data, meta, require) {
+    theme('single-col-fluid', {
         title: 'Asset',
-     	header: [
-            {
-                partial: 'header',
-                context: data
-            }
-        ]
+        header: [{
+            partial: 'header',
+            context: data
+        }],
+        listassets: [{
+            partial: 'list-assets',
+            context: data
+        }]
     });
 };
