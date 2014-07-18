5abccddff16ceeb23ecd8bf9eb83bcc1ee858569
sameeram <mgjaye@gmail.com>
2014-07-18 16:38:41 +0530

    Implemented rendering of unbound table preview

--- modules/apps/publisher/themes/default/theme.js
+++ modules/apps/publisher/themes/default/theme.js
@@ -1,11 +1,10 @@
 var cache = false;
 var log = new Log();
-
-var engine = caramel.engine('handlebars', (function () {
+var engine = caramel.engine('handlebars', (function() {
     return {
-        partials: function (Handlebars) {
+        partials: function(Handlebars) {
             var theme = caramel.theme();
-            var partials = function (file) {
+            var partials = function(file) {
                 (function register(prefix, file) {
                     var i, length, name, files;
                     if (file.isDirectory()) {
@@ -30,10 +29,9 @@ var engine = caramel.engine('handlebars', (function () {
             //Rather register only not overridden partials
             partials(new File(theme.__proto__.resolve.call(theme, 'partials')));
             partials(new File(theme.resolve('partials')));
-
-            Handlebars.registerHelper('dyn', function (options) {
+            Handlebars.registerHelper('dyn', function(options) {
                 var asset = options.hash.asset,
-                    resolve = function (path) {
+                    resolve = function(path) {
                         var p,
                             publisher = require('/modules/publisher.js');
                         if (asset) {
@@ -47,17 +45,112 @@ var engine = caramel.engine('handlebars', (function () {
                 partials(new File(resolve('partials')));
                 return options.fn(this);
             });
-
-            Handlebars.registerHelper('eachField',function(context,options){
-                var ret='';
-                for(var key in context){
-                    context[key].label=context[key].name.label?context[key].name.label:context[key].name.name;
-                    ret+=options.fn(context[key]);
+            Handlebars.registerHelper('eachField', function(context, options) {
+                var ret = '';
+                for (var key in context) {
+                    context[key].label = context[key].name.label ? context[key].name.label : context[key].name.name;
+                    ret += options.fn(context[key]);
                 }
                 return ret;
             });
+            var renderOptionsTextPreview = function(field) {
+                var value;
+                var values = field.value;
+                var output = '';
+                for (var index in values) {
+                    value = values[index];
+                    var delimter = value.indexOf(':')
+                    var option = value.substring(0, delimter);
+                    var text = value.substring(delimter + 1, value.length);
+                    output += '<tr><td>' + option + '</td><td>' + text + '</td></tr>';
+                }
+                return output;
+            };
+            var renderDefaultPreview = function(field) {
+                var label = field.name.label ? field.name.label : field.name.name;
+                return '<td>' + label + '</td><td>' + field.value + '</td>';
+            };
+            Handlebars.registerHelper('renderFieldPreview', function(field) {
+                var out;
+                switch (field.type) {
+                    case 'option-text':
+                        out = renderOptionsTextPreview(field);
+                        break;
+                    default:
+                        out = ''; //renderDefaultPreview(field);
+                        break;
+                }
+                return new Handlebars.SafeString(out);
+            });
+            Handlebars.registerHelper('renderFieldsPreview', function(table) {
+                var heading = (context.subheading) ? context.subheading[0].heading : [];
+                var field;
+                var out = '';
+                for (var index = 0; index < heading.length; index++) {
+                    for (var key in table.fields) {
+                        field = table.fields[key];
+                        out += '<td>' + field.value[index]
+                    }
+                }
+            });
+            Handlebars.registerHelper('renderTable', function(context) {
+                var unboundedPtr = Handlebars.compile('{{> heading_table .}}');
+                var defaultPtr = Handlebars.compile('{{> default_table .}}');
+                var out;
+                var heading = (context.subheading) ? context.subheading[0].heading : [];
+                if (heading.length > 0) {
+                    context.subheading = context.subheading[0].heading;
+                    out = unboundedPtr(context);
+                } else {
+                    out = defaultPtr(context);
+                }
+                return new Handlebars.SafeString(out);
+            });
+            var getHeadings = function(table) {
+                return (table.subheading) ? table.subheading[0].heading : [];
+            };
+            var getNumOfRows = function(table) {
+                for (var key in table.fields) {
+                    return table.fields[key].value.length;
+                }
+                return 0;
+            }
+            Handlebars.registerHelper('renderUnboundTablePreview', function(table) {
+                //Get the number of rows in the table
+                var rowCount = getNumOfRows(table);
+                var fields = table.fields;
+                var out = '';
+                for (var index = 0; index < rowCount; index++) {
+                    out += '<tr>';
+                    for (var key in fields) {
+                        out += '<td>' + fields[key].value[index] + '</td>';
+                    }
+                    out += '</tr>';
+                }
+                return new Handlebars.SafeString(out);
+            });
+            Handlebars.registerHelper('renderTablePreview', function(table) {
+                var headingPtr = Handlebars.compile('{{> heading_table .}}');
+                var defaultPtr = Handlebars.compile('{{> default_table .}}');
+                var unboundPtr = Handlebars.compile('{{> unbound_table .}}');
+                var headings = getHeadings(table);
+                //Check if the table is unbounded
+                if ((table.maxoccurs) && (table.maxoccurs == 'unbounded')) {
+                    if (headings.length > 0) {
+                        table.subheading = table.subheading[0].heading;
+                    }
+                    return new Handlebars.SafeString(unboundPtr(table));
+                }
+                //Check if the table has headings
+                if (headings.length > 0) {
+                    table.subheading = table.subheading[0].heading;
+                    return new Handlebars.SafeString(headingPtr(table));
+                }
+                //Check if the table is a normal table
+                return new Handlebars.SafeString(defaultPtr(table));
+            });
         },
-        render: function (data, meta) {
+        render: function(data, meta) {
             if (request.getParameter('debug') == '1') {
                 response.addHeader("Content-Type", "application/json");
                 print(stringify(data));
@@ -65,7 +158,7 @@ var engine = caramel.engine('handlebars', (function () {
                 this.__proto__.render.call(this, data, meta);
             }
         },
-        globals: function (data, meta) {
+        globals: function(data, meta) {
             var publisher = require('/modules/publisher.js'),
                 user = require('store').server.current(meta.session);
             return 'var store = ' + stringify({
@@ -74,11 +167,10 @@ var engine = caramel.engine('handlebars', (function () {
         }
     };
 }()));
-
-var resolve = function (path) {
-    var themeResolver=this.__proto__.resolve;
-    var asset=require('rxt').asset;
-    path=asset.resolve(request,path,this.name,this,themeResolver);
+var resolve = function(path) {
+    var themeResolver = this.__proto__.resolve;
+    var asset = require('rxt').asset;
+    path = asset.resolve(request, path, this.name, this, themeResolver);
     /*var p,
         publisher = require('/modules/publisher.js'),
         asset = publisher.currentAsset();
