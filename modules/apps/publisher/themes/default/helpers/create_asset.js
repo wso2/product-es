37202a6d35bd52d1a1cc03b43d2baf29cc4cd5d6
sameeram <mgjaye@gmail.com>
2014-07-22 16:40:01 +0530

    Added jquery form framework

---
+++ modules/apps/publisher/themes/default/helpers/create_asset.js
@@ -0,0 +1,6 @@
+var resources = function (page, meta) {
+    return {
+       	js:['libs/jquery.form.min.js','create_asset.js'], 
+       	css:['bootstrap-select.min.css','datepick/smoothness.datepick.css']
+    };
+};
