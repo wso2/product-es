3c8ed96821918201ab680c947c75ae04762c2805
sameeram <mgjaye@gmail.com>
2014-07-25 12:08:15 +0530

    Removed warning which is shown even if tenant configs are found

--- modules/jaggery-modules/rxt/module/scripts/ui/ui.js
+++ modules/jaggery-modules/rxt/module/scripts/ui/ui.js
@@ -12,47 +12,44 @@ var ui = {};
             ribbon: {},
             meta: {
                 pageName: options.pageName,
-                currentPage:options.currentPage,
+                currentPage: options.currentPage,
                 title: 'Empty',
-                landingPage:options.landingPage
+                landingPage: options.landingPage
             }
         }
     };
-    var processPageName=function(suffix){
-        var comps=suffix.split('/');
+    var processPageName = function(suffix) {
+        var comps = suffix.split('/');
         return comps[0];
     };
     var getPageName = function(request) {
         var uriMatcher = new URIMatcher(request.getRequestURI());
         uriMatcher.match('/{context}/asts/{type}/{+suffix}');
         var options = uriMatcher.elements() || {};
-        var page={};
-        page.currentPage=options.pageName;
-        page.pageName=processPageName(options.suffix)
+        var page = {};
+        page.currentPage = options.pageName;
+        page.pageName = processPageName(options.suffix)
         return page;
     }
     ui.buildPage = function(session, request) {
         var server = require('store').server;
-        var userMod=require('store').user;
+        var userMod = require('store').user;
         var user = server.current(session);
-        var tenantId=user.tenantId;
-        var configs=userMod.configs(tenantId);
-        var landingPage='';
-
+        var tenantId = user.tenantId;
+        var configs = userMod.configs(tenantId);
+        var landingPage = '';
         //Determine landing page details
-        if(configs){
-            log.warn('Unable to locate tnenat configurations');
-            if((configs.application)&&(configs.application.landingPage)){
-                landingPage=configs.application.landingPage;
+        if (configs) {
+            if ((configs.application) && (configs.application.landingPage)) {
+                landingPage = configs.application.landingPage;
             }
         }
         var pageDetails = getPageName(request);
-
         var page = genericPage({
             username: user.username,
             pageName: pageDetails.pageName,
-            currentPage:pageDetails.currentPage,
-            landingPage:landingPage
+            currentPage: pageDetails.currentPage,
+            landingPage: landingPage
         });
         return page;
     };
