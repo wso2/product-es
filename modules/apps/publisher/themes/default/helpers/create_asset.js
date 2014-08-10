d4840f3e6e4817b734d70a51a1fbb21aaeb01353
sameeram <mgjaye@gmail.com>
2014-08-10 12:46:42 +0530

    Added styling and option text

--- modules/apps/publisher/themes/default/helpers/create_asset.js
+++ modules/apps/publisher/themes/default/helpers/create_asset.js
@@ -1,6 +1,6 @@
 var resources = function (page, meta) {
     return {
-       	js:['libs/jquery.form.min.js','create_asset.js'], 
+       	js:['libs/jquery.form.min.js','create_asset.js','common/option-text.js'], 
        	css:['bootstrap-select.min.css','datepick/smoothness.datepick.css']
     };
 };
---
+++ modules/apps/publisher/themes/default/js/common/option-text.js
@@ -0,0 +1,53 @@
+var addOptionTextRow = {};
+var removeOptionTextRow = {};
+$(function() {
+	var updateName=function(name,index){
+		var sub=name.substring(0,name.length-1);
+		return sub+index;
+	};
+    var generateId = function(root, row) {
+        var rowCount = $(root).data('rowCount');
+        if(rowCount){
+        	rowCount=1;
+        	$(root).data('rowCount',rowCount);
+        }
+        for(var index=0;index<row.childNodes.length;index++){
+        	var cell=row.childNodes[index];
+        	var element=cell.childNodes[0];
+        	var name=$(element).attr('name');
+        	if(name){
+        		$(element).attr('name',updateName(name, rowCount));
+        	}
+        }
+    };
+    var countRows = function(root) {
+        var count = root.childNodes.length;
+        return count;
+    };
+    addOptionTextRow = function(el) {
+        var root = el.parentNode.parentNode.parentNode;
+        //Check if there is a saved template in the root
+        var template = $(root).data('template');
+        if (template) {
+            generateId(root, template);
+            var clone = $(template).clone();
+        } else {
+            var row = root.childNodes[2];
+            generateId(root, row);
+            var clone = $(row).clone();
+        }
+        clone.appendTo(root);
+    };
+    removeOptionTextRow = function(el) {
+        var row = el.parentNode.parentNode;
+        var root = row.parentNode;
+        var rowCount = $(root).data('rowCount');
+        if (!rowCount) {
+            rowCount = countRows(root);
+            $(root).data('rowCount', rowCount);
+        }
+        $(root).data('template', row);
+        //Cache this row before removal
+        $(row).remove();
+    };
+});
