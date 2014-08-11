d5e756f4b2a3a5e1e3a979f1889c6bde67a77e08
sameeram <mgjaye@gmail.com>
2014-08-11 12:04:59 +0530

    Refactored the option text code to remove duplication

--- modules/apps/publisher/themes/default/js/common/option_text.js
+++ modules/apps/publisher/themes/default/js/common/option_text.js
@@ -1,37 +1,57 @@
+/**
+ * The script is used to handle the rendering of the options text field type.It handles
+ * - Adding of new rows
+ * - Removal of existing rows;
+ * @type {Object}
+ */
 var addOptionTextRow = {};
 var removeOptionTextRow = {};
 $(function() {
+    var ROW_COUNT = 'rowCount'; //A counter which maintains the number of rows created in order to generate unique ids for each row
+    var ROW_TEMPLATE = 'template';
+    /**
+     * The function generates a unique name given the current name of an element and an index
+     * @param  {[type]} name  The current name of the element
+     * @param  {[type]} index A unique index value
+     * @return {[type]}       A unique name based on the current name and the provided index.It can be considered unique for the lifetime of the page
+     */
     var updateName = function(name, index) {
         var sub = name.substring(0, name.length - 1);
         return sub + index;
     };
+    /**
+     * The function resets the contents of the row since the new rows
+     * which are added contain the values of the template
+     * @param  {[type]} row [description]
+     */
     var clearContent = function(row) {
+        iterateRow(row, function(cell) {
+            $(cell.childNodes[0]).val('');
+        })
+    };
+    var iterateRow = function(row, fn) {
         for (var index = 0; index < row.childNodes.length; index++) {
             var cell = row.childNodes[index];
             var element = cell.childNodes[0];
             var name = $(element).attr('name');
             if (name) {
-                $(cell.childNodes[0]).val('');
+                fn(cell);
             }
         }
     };
     var generateId = function(root, row) {
-        var rowCount = $(root).data('rowCount');
+        var rowCount = $(root).data(ROW_COUNT);
         if (!rowCount) {
             rowCount = 1;
-            $(root).data('rowCount', rowCount);
         } else {
             rowCount++;
-            $(root).data('rowCount', rowCount);
         }
-        for (var index = 0; index < row.childNodes.length; index++) {
-            var cell = row.childNodes[index];
+        $(root).data(ROW_COUNT, rowCount);
+        iterateRow(row, function(cell) {
             var element = cell.childNodes[0];
             var name = $(element).attr('name');
-            if (name) {
-                $(element).attr('name', updateName(name, rowCount));
-            }
-        }
+            $(element).attr('name', updateName(name, rowCount));
+        });
     };
     var countRows = function(root) {
         var count = root.childNodes.length;
@@ -40,7 +60,7 @@ $(function() {
     addOptionTextRow = function(el) {
         var root = el.parentNode.parentNode.parentNode;
         //Check if there is a saved template in the root
-        var template = $(root).data('template');
+        var template = $(root).data(ROW_TEMPLATE);
         if (template) {
             generateId(root, template);
             var clone = $(template).clone();
@@ -55,13 +75,13 @@ $(function() {
     removeOptionTextRow = function(el) {
         var row = el.parentNode.parentNode;
         var root = row.parentNode;
-        var rowCount = $(root).data('rowCount');
+        var rowCount = $(root).data(ROW_COUNT);
         if (!rowCount) {
             rowCount = countRows(root);
-            $(root).data('rowCount', rowCount);
+            $(root).data(ROW_COUNT, rowCount);
         }
-        $(root).data('template', row);
         //Cache this row before removal
+        $(root).data(ROW_TEMPLATE, row);
         $(row).remove();
     };
 });
