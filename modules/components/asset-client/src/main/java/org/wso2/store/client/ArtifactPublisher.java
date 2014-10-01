fe8e297619849c27055a6a1a00d47092ea555c40
manoj <manojg@wwso2.com>
2014-10-01 12:27:02 +0530

    add log4j

--- modules/components/asset-client/src/main/java/org/wso2/store/client/ArtifactPublisher.java
+++ modules/components/asset-client/src/main/java/org/wso2/store/client/ArtifactPublisher.java
@@ -1,11 +1,11 @@
 package org.wso2.store.client;
 
 import com.google.code.commons.cli.annotations.CliParser;
+import com.google.code.commons.cli.annotations.ParserException;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import org.apache.commons.cli.BasicParser;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -20,7 +20,6 @@ import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -32,32 +31,38 @@ import java.util.List;
 public class ArtifactPublisher {
 
     protected static final Logger log = Logger.getLogger(ArtifactPublisher.class);
+
     static CloseableHttpClient httpClient = null;
     static HttpPost httppost = null;
     static HttpGet httpGet = null;
-    static HttpEntity reqEntity = null;
-    static Gson gson = null;
     static CloseableHttpResponse response = null;
     static SSLConnectionSocketFactory sslConnectionSocketFactory = null;
     static HttpContext httpContext = null;
+
+    static Gson gson = null;
+
     static HashMap<String, List<String>> rxtFileAttributesMap = null;
 
     static String hostName = "";
     static String port = "";
-
     static String userName = "";
     static String pwd = "";
     static String sessionId = "";
     static String context = "";
     static String location;
 
-    public static void main(String args[]) throws Exception {
-
+    public static void main(String args[]) throws Exception{
 
         CliParser parser = new CliParser(new BasicParser());
+        CliOptions cliOptions = null;
 
+        try {
+            cliOptions = parser.parse(CliOptions.class, args);
 
-        CliOptions cliOptions = parser.parse(CliOptions.class, args);
+        }catch (ParserException parseException){
+            log.error("command line arguments parsing error:", parseException);
+            return;
+        }
 
         hostName = cliOptions.getHostName();
         port = cliOptions.getPort();
@@ -68,62 +73,58 @@ public class ArtifactPublisher {
 
         File samplesDirectory = new File(location);
 
-
         try {
-
             SSLContextBuilder builder = new SSLContextBuilder();
             builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
             sslConnectionSocketFactory = new SSLConnectionSocketFactory(builder.build());
 
             httpContext = new BasicHttpContext();
-
             sessionId = getSession(sslConnectionSocketFactory, httpContext, hostName, port, userName, pwd);
 
-            String[] rxtArr = getRxtTypes();
-
-            rxtFileAttributesMap = new HashMap<String, List<String>>();
-
-            for (String rxtType : rxtArr) {
-                rxtFileAttributesMap.put(rxtType, getAttributesForType(rxtType, "file"));
-            }
-
-            readAssets(samplesDirectory);
-
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        }catch (Exception ex){
+            log.error("logging session fetching error");
+            return;
         }
 
+        String[] rxtArr = null;
+        try {
+             rxtArr = getRxtTypes();
+
+        }catch (Exception ex){
+            log.error("Error in get rxt types");
+            return;
+
+        }
+
+        rxtFileAttributesMap = new HashMap<String, List<String>>();
+        for (String rxtType : rxtArr) {
+            rxtFileAttributesMap.put(rxtType, getAttributesForType(rxtType, "file"));
+        }
+        readAssets(samplesDirectory);
     }
 
 
     private static String getSession(SSLConnectionSocketFactory sslsf, HttpContext httpContext, String hostName, String port, String userName, String pwd) throws Exception {
 
         gson = new Gson();
-
         String authUrl = "https://" + hostName + ":" + port + "/" + context + IConstants.PUBLISHER_AUTHORIZATION_URL + "?username=" + userName + "&password=" + pwd;
 
         httppost = new HttpPost(authUrl);
+        httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
 
-        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-
-        String sessionid = "";
-
+        String sessionId = "";
         response = null;
-
 
         try {
 
-            response = httpclient.execute(httppost, httpContext);
+            response = httpClient.execute(httppost, httpContext);
 
         } catch (ClientProtocolException clientProtocolException) {
-
-            clientProtocolException.printStackTrace();
-            throw new Exception(clientProtocolException.getMessage(),
-                    clientProtocolException);
+            log.error("client protocol exception in logging"+authUrl, clientProtocolException);
+            throw new Exception(clientProtocolException.getMessage(), clientProtocolException);
 
         } catch (IOException ioException) {
-            ioException.printStackTrace();
+            log.error("io error in logging"+authUrl, ioException);
             throw new Exception(ioException.getMessage(), ioException);
 
         }
@@ -133,69 +134,67 @@ public class ArtifactPublisher {
             Authorize authorize = gson.fromJson(responseJson, Authorize.class);
 
             if (authorize.getData() != null) {
+                sessionId = authorize.getData().getSessionId();
+                log.info("Logged:" + sessionId);
 
-                sessionid = authorize.getData().getSessionId();
-                log.info("Logged:" + sessionid);
             } else {
                 log.info("login failure!!!" + responseJson);
+
             }
 
         } catch (IOException ioException) {
-            // TODO Auto-generated catch block
-            ioException.printStackTrace();
+            log.error(ioException);
             throw new Exception(ioException.getMessage(), ioException);
 
         } finally {
-            httpclient.close();
+            httpClient.close();
         }
 
-        return sessionid;
+        return sessionId;
     }
 
 
-    private static String[] getRxtTypes() {
-
+    private static String[] getRxtTypes() throws Exception{
 
         String apiUrl = "https://" + hostName + ":" + port + "/" + context + IConstants.RXT_URL;
-        httpGet = new HttpGet(apiUrl);
 
-        CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
+        httpGet = new HttpGet(apiUrl);
+        httpClient = HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
         response = null;
 
+        try{
+            response = httpClient.execute(httpGet, httpContext);
 
-        try {
+        }catch(ClientProtocolException clientProtocolException) {
+              log.error("client protocol exception connecting to RXT API:"+apiUrl, clientProtocolException);
+              throw new Exception(clientProtocolException);
 
-            response = httpclient.execute(httpGet, httpContext);
-
-        } catch (ClientProtocolException clientProtocolException) {
-
-            clientProtocolException.printStackTrace();
-
-        } catch (IOException ioException) {
-
+        }catch(IOException ioException) {
+              log.error("error connecting RXT API:"+apiUrl, ioException);
+              throw new Exception(ioException);
         }
 
         String responseJson = null;
         String[] arrRxt = null;
+
         try {
             responseJson = EntityUtils.toString(response.getEntity());
             arrRxt = gson.fromJson(responseJson, String[].class);
 
         } catch (Exception ioException) {
             ioException.printStackTrace();
+
         } finally {
             try {
-                httpclient.close();
+                httpClient.close();
             } catch (IOException e) {
-                e.printStackTrace();
+
             }
         }
         return arrRxt;
-
-
     }
 
-    private static List<String> getAttributesForType(String rxtType, String type) {
+    private static List<String> getAttributesForType(String rxtType, String type) throws Exception{
 
         String apiUrl = "https://" + hostName + ":" + port + "/" + context + IConstants.RXT_ATTRIBUTES_FOR_GIVEN_TYPE + "/" + rxtType + "/" + type;
         httpGet = new HttpGet(apiUrl);
@@ -210,11 +209,12 @@ public class ArtifactPublisher {
             response = httpclient.execute(httpGet, httpContext);
 
         } catch (ClientProtocolException clientProtocolException) {
-
-            clientProtocolException.printStackTrace();
-
+            log.error("client protocol error in get RXT attributes for rxt type"+apiUrl, clientProtocolException);
+            throw new Exception(clientProtocolException);
 
         } catch (IOException ioException) {
+            log.error("io error in get RXT attributes for rxt type"+apiUrl, ioException);
+            throw new Exception(ioException);
 
         }
 
@@ -227,26 +227,21 @@ public class ArtifactPublisher {
             attrArr = gson.fromJson(responseJson, String[].class);
 
         } catch (Exception ioException) {
-            // TODO Auto-generated catch block
-            ioException.printStackTrace();
-            //throw new Exception(ioException.getMessage(), ioException);
+            log.error("io error in get RXT attributes for rxt type"+apiUrl, ioException);
+            throw new Exception(ioException);
 
         } finally {
             try {
                 httpclient.close();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
         }
-
-
         return Arrays.asList(attrArr);
 
     }
 
 
-    private static void readAssets(File dir) throws Exception {
+    private static void readAssets(File dir)  {
 
         Asset[] assetArr = null;
         BufferedReader br = null;
@@ -255,12 +250,17 @@ public class ArtifactPublisher {
         for (final File file : dir.listFiles()) {
 
             if (file.isFile() && file.getName().substring(file.getName().lastIndexOf(".") + 1).equals("json")) {
-                br = new BufferedReader(new FileReader(file));
-                JsonParser parser = new JsonParser();
-                JsonArray Jarray = (JsonArray) parser.parse(br).getAsJsonObject().get("assets");
+                try {
+                    br = new BufferedReader(new FileReader(file));
+                    JsonParser parser = new JsonParser();
+                    JsonArray jarray = (JsonArray) parser.parse(br).getAsJsonObject().get("assets");
 
-                assetArr = gson.fromJson(Jarray, Asset[].class);
-                uploadAssets(assetArr, dir);
+                    assetArr = gson.fromJson(jarray, Asset[].class);
+                    uploadAssets(assetArr, dir);
+
+                }catch(Exception ex){
+                    log.error("file not completely uploaded"+file.getName());
+                }
             }
 
             if (file.list() != null && file.list().length > 0) {
@@ -278,60 +278,48 @@ public class ArtifactPublisher {
 
         File imageFile = null;
         String responseJson = null;
+        StringBuffer publisherUrlBuff = null;
 
         for (Asset asset : assetArr) {
-
-            StringBuffer publisherUrlBuff = new StringBuffer();
-
-            if (asset.getId() != null) {
-
-                publisherUrlBuff.append("https://" + hostName + ":" + port + "/" + context + IConstants.PUBLISHER_URL + "/" + asset.getId() + "?type=" + asset.getType());
-            } else {
-
-                publisherUrlBuff.append("https://" + hostName + ":" + port + "/" + context + IConstants.PUBLISHER_URL + "?type=" + asset.getType());
-            }
-
-
+             publisherUrlBuff = new StringBuffer();
+             if (asset.getId() != null) {
+                 publisherUrlBuff.append("https://" + hostName + ":" + port + "/" + context + IConstants.PUBLISHER_URL + "/" + asset.getId() + "?type=" + asset.getType());
+             } else {
+                 publisherUrlBuff.append("https://" + hostName + ":" + port + "/" + context + IConstants.PUBLISHER_URL + "?type=" + asset.getType());
+             }
             multiPartBuilder = MultipartEntityBuilder.create();
             multiPartBuilder.addTextBody("sessionId", sessionId);
             multiPartBuilder.addTextBody("asset", gson.toJson(asset));
 
-
             attrMap = asset.getAttributes();
 
             for (String attrKey : attrMap.keySet()) {
-
                 httpClient = HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
                 httppost = new HttpPost(publisherUrlBuff.toString());
-
                 fileAttributes = rxtFileAttributesMap.get(asset.getType());
+
                 if (fileAttributes != null && fileAttributes.contains(attrKey)) {
                     imageFile = new File(dir + File.separator + IConstants.RESOURCE_DIR_NAME + File.separator + attrMap.get(attrKey));
                     multiPartBuilder.addBinaryBody(attrKey, imageFile);
-
                     httppost.setEntity(multiPartBuilder.build());
-
                 }
             }
-
-
             try {
                 response = httpClient.execute(httppost, httpContext);
                 responseJson = EntityUtils.toString(response.getEntity());
-                System.out.println(responseJson.toString());
                 log.info(responseJson.toString());
+
             } catch (Exception ex) {
-                log.error("error in asssetUpload", ex);
+                log.error(asset);
+                log.error("error in asset Upload", ex);
             } finally {
                 try {
                     httpClient.close();
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
                 }
             }
-        }
 
+        }
     }
 
 
