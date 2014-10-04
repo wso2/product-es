ab58a12601b1e4e52c8d792fdaf8d616f04a5d7c
Manu Perera <manu@wso2.com>
2014-10-04 19:27:48 -0400

    import static

--- modules/components/social/org.wso2.carbon.social.sql/src/main/java/org/wso2/carbon/social/sql/SQLActivityPublisher.java
+++ modules/components/social/org.wso2.carbon.social.sql/src/main/java/org/wso2/carbon/social/sql/SQLActivityPublisher.java
@@ -16,112 +16,114 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import static org.wso2.carbon.social.sql.Constants.*;
+
 public class SQLActivityPublisher extends ActivityPublisher {
 
-	private static Log log = LogFactory.getLog(SQLActivityPublisher.class);
+    private static Log log = LogFactory.getLog(SQLActivityPublisher.class);
 
     private JsonParser parser = new JsonParser();
 
-	@Override
-	protected String publish(String id, NativeObject activity) {
-		DSConnection con = new DSConnection();
-		Connection connection = con.getConnection();
-		if(connection != null){
-			PreparedStatement statement = null;
-			try{
-				String json = JSONUtil.SimpleNativeObjectToJson(activity);
-				String contextId = JSONUtil.getNullableProperty(activity, Constants.CONTEXT_JSON_PROP, Constants.ID_JSON_PROP);
-				String userId = JSONUtil.getNullableProperty(activity, Constants.ACTOR_JSON_PROP, Constants.ID_JSON_PROP);
-				String rating = JSONUtil.getNullableProperty(activity, Constants.OBJECT_JSON_PROP, Constants.RATING_JSON_PROP);
-				if (contextId == null) {
-					contextId = JSONUtil.getProperty(activity, Constants.TARGET_JSON_PROP, Constants.ID_JSON_PROP);
-				}
-				if (userId == null) {
-					userId = JSONUtil.getProperty(activity, Constants.ACTOR_JSON_PROP, Constants.ID_JSON_PROP);
-				}
-				if (rating == null) {
-					rating = JSONUtil.getProperty(activity, Constants.OBJECT_JSON_PROP, Constants.RATING_JSON_PROP);
-				}
+    @Override
+    protected String publish(String id, NativeObject activity) {
+        DSConnection con = new DSConnection();
+        Connection connection = con.getConnection();
+        if (connection != null) {
+            PreparedStatement statement = null;
+            try {
+                String json = JSONUtil.SimpleNativeObjectToJson(activity);
+                String contextId = JSONUtil.getNullableProperty(activity, CONTEXT_JSON_PROP, ID_JSON_PROP);
+                String userId = JSONUtil.getNullableProperty(activity, ACTOR_JSON_PROP, ID_JSON_PROP);
+                String rating = JSONUtil.getNullableProperty(activity, OBJECT_JSON_PROP, RATING_JSON_PROP);
+                if (contextId == null) {
+                    contextId = JSONUtil.getProperty(activity, TARGET_JSON_PROP, ID_JSON_PROP);
+                }
+                if (userId == null) {
+                    userId = JSONUtil.getProperty(activity, ACTOR_JSON_PROP, ID_JSON_PROP);
+                }
+                if (rating == null) {
+                    rating = JSONUtil.getProperty(activity, OBJECT_JSON_PROP, RATING_JSON_PROP);
+                }
 
-				String votesStr = "{" +
-						"\"upVotes\" : 0, " +
-								"\"downVotes\"  : 0 " +
-								"}";
+                String votesStr = "{" +
+                        "\"upVotes\" : 0, " +
+                        "\"downVotes\"  : 0 " +
+                        "}";
 
-								connection.setAutoCommit(false);
-								statement = connection.prepareStatement(Constants.INSERT_COMMENT_SQL);
-								statement.setString(1, id);
-								statement.setString(2, contextId);
-								statement.setString(3, json);
-								statement.setString(4, userId);
-								statement.setString(5, votesStr);
-								statement.setInt(6, Integer.parseInt(rating));
-								statement.setTimestamp(7,getCurrentTimeStamp());
+                connection.setAutoCommit(false);
+                statement = connection.prepareStatement(INSERT_COMMENT_SQL);
+                statement.setString(1, id);
+                statement.setString(2, contextId);
+                statement.setString(3, json);
+                statement.setString(4, userId);
+                statement.setString(5, votesStr);
+                statement.setInt(6, Integer.parseInt(rating));
+                statement.setTimestamp(7, getCurrentTimeStamp());
 
-								int ret = statement.executeUpdate();
-								connection.commit();
-								if(ret > 0){
-									return id;
-								}else{
-									log.info("failed to publish social event.");
-								}
-			}catch(Exception e){
-				log.error("failed to publish social event.", e);
-			}finally{
-				con.closeConnection(connection);
-			}
-		}
-		return null;
-	}
+                int ret = statement.executeUpdate();
+                connection.commit();
+                if (ret > 0) {
+                    return id;
+                } else {
+                    log.info("failed to publish social event.");
+                }
+            } catch (Exception e) {
+                log.error("failed to publish social event.", e);
+            } finally {
+                con.closeConnection(connection);
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public boolean vote(String commentId, String actorId, int vote) {
-		DSConnection con = new DSConnection();
-		Connection connection = con.getConnection();
-		if(connection != null){
-			PreparedStatement statement = null;
-			PreparedStatement update = null;
-			ResultSet resultSet = null;
-			try{
-				statement = connection.prepareStatement(Constants.SELECT_VOTE_SQL);
-				statement.setString(1, commentId);
-				resultSet = statement.executeQuery();
-				resultSet.next();
+    @Override
+    public boolean vote(String commentId, String actorId, int vote) {
+        DSConnection con = new DSConnection();
+        Connection connection = con.getConnection();
+        if (connection != null) {
+            PreparedStatement statement = null;
+            PreparedStatement update = null;
+            ResultSet resultSet = null;
+            try {
+                statement = connection.prepareStatement(SELECT_VOTE_SQL);
+                statement.setString(1, commentId);
+                resultSet = statement.executeQuery();
+                resultSet.next();
 
-				JsonObject votes = (JsonObject) parser.parse(resultSet.getString(Constants.VOTE_COLUMN));
+                JsonObject votes = (JsonObject) parser.parse(resultSet.getString(VOTE_COLUMN));
 
-				//up-vote scenario
-				if(vote == 1){
-					JsonElement upVotes = votes.get("upVotes");
-					int up = upVotes.getAsInt();
-					up = up + 1;
-					votes.addProperty("upVotes", up);
-				}else{//down-vote scenario
-					JsonElement downVotes = votes.get("downVotes");
-					int down = downVotes.getAsInt();
-					down = down + 1;
-					votes.addProperty("downVotes", down);
-				}
-				String votesStr = votes.toString();
+                //up-vote scenario
+                if (vote == 1) {
+                    JsonElement upVotes = votes.get("upVotes");
+                    int up = upVotes.getAsInt();
+                    up = up + 1;
+                    votes.addProperty("upVotes", up);
+                } else {//down-vote scenario
+                    JsonElement downVotes = votes.get("downVotes");
+                    int down = downVotes.getAsInt();
+                    down = down + 1;
+                    votes.addProperty("downVotes", down);
+                }
+                String votesStr = votes.toString();
 
-				update = connection.prepareStatement(Constants.UPDATE_VOTE_SQL);
-				update.setString(1, votesStr);
-				update.setString(2, commentId);
-				update.executeUpdate();
+                update = connection.prepareStatement(UPDATE_VOTE_SQL);
+                update.setString(1, votesStr);
+                update.setString(2, commentId);
+                update.executeUpdate();
 
-			}catch(Exception e){
-				log.error("Can't retrieve activities from SQL.", e);
-			}finally{
-				con.closeConnection(connection);
-			}
-		}
-		return true;
-	}
+            } catch (Exception e) {
+                log.error("Can't retrieve activities from SQL.", e);
+            } finally {
+                con.closeConnection(connection);
+            }
+        }
+        return true;
+    }
 
-	private static Timestamp getCurrentTimeStamp() {
-		Date today = new Date();
-		return new Timestamp(today.getTime());
-	}
+    private static Timestamp getCurrentTimeStamp() {
+        Date today = new Date();
+        return new Timestamp(today.getTime());
+    }
 
 
 }
