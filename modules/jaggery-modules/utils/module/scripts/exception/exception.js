cf4e8bd47f4182c0235b0478c2e16c9da26bc8ef
Ayesha <ayeshad.09@cse.mrt.ac.lk>
2014-10-09 16:08:01 +0530

    updated

--- modules/jaggery-modules/utils/module/scripts/exception/exception.js
+++ modules/jaggery-modules/utils/module/scripts/exception/exception.js
@@ -1,21 +1,19 @@
 /*
- *  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) WSO2 Inc. (http://wso2.com) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 /**
  * Description: The response of the currently invoked api enpoint is organized
  */
@@ -24,7 +22,12 @@ var exception = {};
 var log = new Log('exception_module');
 
 (function(exception) {
-
+    /**
+     *
+     * @param message The exception description
+     * @param code      HTTP STATUS CODE related to the exception
+     * @return The error object
+     */
     exception.buildExceptionObject = function(message, code) {
         var error = {};
         error.message = message;
