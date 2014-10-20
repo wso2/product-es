0417b584f0c6f58ac4638c915c9048d0285872cd
manoj <manojg@wwso2.com>
2014-10-20 14:23:47 +0530

    bam client

--- modules/jaggery-modules/bampublsiher/module/scripts/bampublisher.js
+++ modules/jaggery-modules/bampublsiher/module/scripts/bampublisher.js
@@ -1,21 +1,46 @@
-/**
- * Following module act as a client to publish events to BAM
- * @type {{}}
+/*
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 var bamclient = {};
 
 (function (bamclient) {
 
-    var bamPublsiher = Packages.org.wso2.stroe.bamclient,
+    var Publisher = Packages.org.wso2.store.bamclient.EventPublisher,
         carbon = require('carbon'),
         log = new Log();
 
-    var eventStreamDefinition = '{"name":"asseteventsStream","version":"1.0.0","nickName":"asseteventsStream","description":"assets events stream","metaData":["name":"clientType","type":"STRING"],"payloadData":[{"name":"event","type":"STRING"},{"name":"assetId","type":"STRING"},{"name":"assetType","type":"STRING"},{"name":"assetName","type":"STRING"},{"name":"description","type":"STRING"}]}';
+    var server = require('store').server;
 
-     bamclient.publishEvents = function (eventName, assetId, assetType,assetName,decription) {
+    var eventStreamDefinition = '{"name":"asseteventsStreamNew1","version":"1.0.0","nickName":"asseteventsStream","description":"assets events stream","metaData":[{"name":"clientType","type":"STRING"}],"payloadData":[{"name":"user","type":"STRING"},{"name":"tenant","type":"STRING"},{"name":"event","type":"STRING"},{"name":"assetId","type":"STRING"},{"name":"assetType","type":"STRING"},{"name":"assetName","type":"STRING"},{"name":"description","type":"STRING"}]}';
+    bamclient.publishEvents = function (eventName, assetId, assetType, assetName, description) {
+
+        var user = server.current(session);
+        log.info("user" + user);
+        var tenantId = -1234;
+
+        if (user != null) {
+            tenantId = user.tenantId;
+        }
+
+        log.info("tenantId:" + tenantId);
         var streamdefObj = parse(eventStreamDefinition);
-        bamPublsiher.getInstance().publishEvents(streamdefObj.name,streamdefObj.version, eventStreamDefinition, streamdefObj.metaData,{eventName,assetId,assetType, assetName, decription});
+        Publisher.getInstance().publishEvents(streamdefObj.name, streamdefObj.version, eventStreamDefinition, stringify(streamdefObj.metaData), new Array(user.username, tenantId, eventName, assetId, assetType, assetName, description));
     };
+
 
 }(bamclient));
