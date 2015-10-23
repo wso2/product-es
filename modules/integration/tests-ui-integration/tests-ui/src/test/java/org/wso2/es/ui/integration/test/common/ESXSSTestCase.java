9beb588ed3df2fc7b9a0cf4d02e602c241544b3a
Lasantha Samarakoon <lasn1987@gmail.com>
2015-10-23 15:05:31 +0530

    Added UI integration tests to test script injection in SSO Login page

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/es/ui/integration/test/common/ESXSSTestCase.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/es/ui/integration/test/common/ESXSSTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2011 WSO2, Inc. (http://wso2.com)
+ * Copyright 2005-2015 WSO2, Inc. (http://wso2.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,63 +24,166 @@ import org.wso2.carbon.automation.extensions.selenium.BrowserManager;
 import org.wso2.es.ui.integration.util.BaseUITestCase;
 import org.wso2.es.ui.integration.util.ESWebDriver;
 
+import javax.xml.xpath.XPathExpressionException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.StringTokenizer;
+
 import static org.testng.Assert.assertFalse;
 
 /**
- * Created by lasanthas on 10/21/15.
+ * Test script injection in user user registration, login, and gadgets page
  */
 public class ESXSSTestCase extends BaseUITestCase {
+
+    private static final String STORE_LOGIN_URL = "/store/login";
+    private static final String SSO_LOGIN_URL = "/sso/login";
+
+    private String baseHttpsUrl;
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
         super.init();
         driver = new ESWebDriver(BrowserManager.getWebDriver());
         baseUrl = getWebAppURL();
+        baseHttpsUrl = getWebAppURLHttps();
     }
 
     @Test(groups = "wso2.es.common", description = "Testing XSS in user registration")
     public void testRegisterUserXSSTestCase() throws Exception {
 
-        String xssString = "XSS";
+        String url = baseHttpsUrl + "/sso/register_new_user?relyingParty=";
 
-        String url = getWebAppURLHttps() + "/sso/register_new_user?" +
-                "relyingParty=\"><script>alert(\"" + xssString + "\")</script>";
-        driver.get(url);
+        String alertMessage = "XSS";
+        driver.get(url + "\"><script>alert(\"" + alertMessage + "\")</script>");
 
-        boolean xssSuccess = false;
+        boolean scriptInjected = false;
 
         try {
-            String result = closeAlertAndGetItsText(driver, false);
+            String actualAlertMessage = closeAlertAndGetItsText(driver, false);
 
-            if (result.equals(xssString))
-                xssSuccess = true;
+            if (actualAlertMessage.equals(alertMessage))
+                scriptInjected = true;
 
         } catch (NoAlertPresentException ex) {
         }
 
-        assertFalse(xssSuccess, "Possible cross site scripting detected.");
+        assertFalse(scriptInjected, "Script injected via the query string");
     }
 
-    @Test(groups = "wso2.es.store", description = "Test XSS in Gadgets Page")
+    @Test(groups = "wso2.es.common", description = "Test XSS in Gadgets Page")
     public void testListGadgetsXSSTestCase() throws Exception {
 
-        String xssString = "XSS";
+        String alertMessage = "XSS";
 
         String url = baseUrl + "/store/assets/gadget/list?" +
-                "sortBy=overview_name&sort=}</script><script>alert('" + xssString + "')</script>";
+                "sortBy=overview_name&sort=}</script><script>alert('" + alertMessage + "')</script>";
         driver.get(url);
 
-        boolean xssSuccess = false;
+        boolean scriptInjected = false;
 
         try {
-            String result = closeAlertAndGetItsText(driver, false);
+            String actualAlertMessage = closeAlertAndGetItsText(driver, false);
 
-            if (result.equals(xssString))
-                xssSuccess = true;
+            if (actualAlertMessage.equals(alertMessage))
+                scriptInjected = true;
         } catch (NoAlertPresentException ex) {
         }
 
-        assertFalse(xssSuccess, "Possible cross site scripting detected");
+        assertFalse(scriptInjected, "Script injected via the query string");
+    }
+
+    @Test(groups = "wso2.es.common", description = "Test XSS in SSO Login")
+    public void testSSOLoginXSSTestCase() throws MalformedURLException, XPathExpressionException {
+
+        String alertMessage = "XSS";
+
+        String malformedRelayStateQueryString = "?";
+        String malformedSessionDataKeyQueryString = "?";
+
+        boolean scriptInjected;
+
+        driver.get(baseHttpsUrl + STORE_LOGIN_URL);
+        URL url = new URL(driver.getCurrentUrl());
+
+
+        // Get the current URL, fetch the query string and slice it by '&' and '=' signs to access
+        // the each parameter key-value pair.
+        StringTokenizer queryStringTokenizer = new StringTokenizer(url.getQuery(), "&", false);
+        while (queryStringTokenizer.hasMoreElements()) {
+
+            String param = queryStringTokenizer.nextToken();
+
+            StringTokenizer parameterTokenizer = new StringTokenizer(param, "=", false);
+
+            String paramName = "";
+            if (parameterTokenizer.hasMoreElements())
+                paramName = parameterTokenizer.nextToken();
+
+            // If relayState and the SessionDataKey parameters, modify them accordingly and
+            // create separate query strings
+            if (paramName.toLowerCase().equals("relaystate") ||
+                    paramName.toLowerCase().equals("sessiondatakey")) {
+
+                String paramValue = "";
+                if (parameterTokenizer.hasMoreElements())
+                    paramValue = parameterTokenizer.nextToken();
+
+                String modifiedParam = paramName + "=" + paramValue +
+                        "\"><script>alert('" + alertMessage + "');</script>";
+
+                if (paramName.toLowerCase().equals("relaystate")) {
+
+                    malformedRelayStateQueryString += modifiedParam + "&";
+                    malformedSessionDataKeyQueryString += param + "&";
+
+                } else {
+
+                    malformedRelayStateQueryString += param + "&";
+                    malformedSessionDataKeyQueryString += modifiedParam + "&";
+
+                }
+
+            } else {
+
+                // Append the all other parameters as they are in the new query strings
+                malformedRelayStateQueryString += param + "&";
+                malformedSessionDataKeyQueryString += param + "&";
+
+            }
+        }
+
+        // Test the relay state parameter
+        scriptInjected = false;
+
+        driver.get(baseHttpsUrl + SSO_LOGIN_URL + malformedRelayStateQueryString);
+
+        try {
+            String actualAlertMessage = closeAlertAndGetItsText(driver, false);
+
+            if (actualAlertMessage.equals(alertMessage))
+                scriptInjected = true;
+
+        } catch (NoAlertPresentException ex) {
+        }
+
+        assertFalse(scriptInjected, "Script injected to relay state via the query string");
+
+        // Test the session data key parameter
+        scriptInjected = false;
+
+        driver.get(baseHttpsUrl + SSO_LOGIN_URL + malformedSessionDataKeyQueryString);
+
+        try {
+            String actualAlertMessage = closeAlertAndGetItsText(driver, false);
+
+            if (actualAlertMessage.equals(alertMessage))
+                scriptInjected = true;
+
+        } catch (NoAlertPresentException ex) {
+        }
+
+        assertFalse(scriptInjected, "Script injected to session data key via the query string");
     }
 
     @AfterClass(alwaysRun = true)
