a42f83212c22a3d53dd1cf612ed9653c7d98234f
denuwanthi <denuwanthi.hasanthika@gmail.com>
2016-05-11 16:51:37 +0530

    STORE-1285 ES migration client

---
+++ modules/distribution/src/resources/es-migration-client/src/main/java/org/wso2/carbon/es/migration/client/MigrateFrom200to210.java
@@ -0,0 +1,113 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.carbon.es.migration.client;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.base.MultitenantConstants;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.es.migration.EsMigrationException;
+import org.wso2.carbon.es.migration.client.internal.ServiceHolder;
+import org.wso2.carbon.es.migration.util.Constants;
+import org.wso2.carbon.registry.core.Registry;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.user.api.Tenant;
+import org.wso2.carbon.user.api.UserStoreException;
+import org.wso2.carbon.user.core.tenant.TenantManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.xml.stream.XMLStreamException;
+
+public class MigrateFrom200to210 implements MigrationClient{
+
+    private static final Log log = LogFactory.getLog(MigrateFrom200to210.class);
+
+    @Override
+    public void cleanOldResources() throws EsMigrationException {
+        List<Tenant> tenantsArray = null;
+        try {
+            long startTimeMillis = System.currentTimeMillis();
+            log.info("Store configuration migration started.");
+            tenantsArray = getTenantsArray();
+            for (Tenant tenant : tenantsArray) {
+                clean(tenant);
+            }
+            log.info("Migration for store config Completed Successfully in " +
+                     (System.currentTimeMillis() - startTimeMillis)
+                     + "ms");
+        } catch (UserStoreException e) {
+            String msg = "Error occurred while searching for tenant admin. ";
+            log.error(msg, e);
+            throw new EsMigrationException(msg, e);
+        }catch (RegistryException e) {
+            String msg = "Error occurred while performing registry operation. ";
+            log.error(msg, e);
+            throw new EsMigrationException(msg, e);
+        } catch (XMLStreamException e) {
+            String msg = "Error while processing string to xml";
+            log.error(msg, e);
+            throw new EsMigrationException(msg, e);
+        }
+    }
+
+    /**
+     * This method returns the list of tenants.
+     * @return list of tenants
+     * @throws org.wso2.carbon.user.api.UserStoreException
+     */
+    private List<Tenant> getTenantsArray() throws UserStoreException {
+        TenantManager tenantManager = ServiceHolder.getRealmService().getTenantManager();
+        List<Tenant> tenantsArray = new ArrayList<Tenant>(Arrays.asList(tenantManager.getAllTenants()));
+        Tenant superTenant = new Tenant();
+        superTenant.setDomain(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME);
+        superTenant.setId(MultitenantConstants.SUPER_TENANT_ID);
+        tenantsArray.add(superTenant);
+        return tenantsArray;
+    }
+
+    /**
+     * This method removes the store.json file at config registry which will fix issue REGISTRY-3528
+     * @param tenant tenant
+     * @throws UserStoreException
+     * @throws RegistryException
+     * @throws XMLStreamException
+     */
+    private void clean(Tenant tenant) throws UserStoreException, RegistryException, XMLStreamException {
+
+        int tenantId = tenant.getId();
+        try {
+            PrivilegedCarbonContext.startTenantFlow();
+            PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(tenant.getDomain(), true);
+            PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(tenantId);
+            String adminName = ServiceHolder.getRealmService().getTenantUserRealm(tenantId).getRealmConfiguration()
+                    .getAdminUserName();
+            PrivilegedCarbonContext.getThreadLocalCarbonContext().setUsername(adminName);
+            ServiceHolder.getTenantRegLoader().loadTenantRegistry(tenantId);
+            Registry registry = ServiceHolder.getRegistryService().getConfigUserRegistry(adminName,tenantId);
+            if(registry.resourceExists(Constants.STORE_CONFIG_PATH)){
+                registry.delete(Constants.STORE_CONFIG_PATH);
+            }
+        } finally {
+            PrivilegedCarbonContext.endTenantFlow();
+        }
+
+    }
+}
---
+++ modules/distribution/src/resources/es-migration-client/src/main/java/org/wso2/carbon/es/migration/util/Constants.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.carbon.es.migration.util;
+
+import java.io.File;
+
+public class Constants {
+
+    public static final String VERSION_210 = "2.1.0";
+    //Constants for email username migration client
+    public static final String METADATA_NAMESPACE = "http://www.wso2.org/governance/metadata";
+    public static final String OLD_EMAIL_AT_SIGN = ":";
+    public static final String NEW_EMAIL_AT_SIGN = "-at-";
+    public static final String CARBON_HOME = System.getProperty("carbon.home");
+    public static final String REGISTRY_XML_PATH = Constants.CARBON_HOME + File.separator + "repository" + File.separator
+                                                   + "conf" + File.separator + "registry.xml";
+    public static final String UM_MIGRATION_SCRIPT = "/migration-scripts/um_migration.sql";
+    public static final String REGISTRY_MIGRATION_SCRIPT = "/migration-scripts/reg_migration.sql";
+    public static final String GOV_PATH = "/_system/governance";
+    public static final String RESOURCETYPES_RXT_PATH =
+            GOV_PATH + "/repository/components/org.wso2.carbon.governance/types";
+    public static final String ARTIFACT_TYPE = "artifactType";
+    public static final String FILE_EXTENSION = "fileExtension";
+    public static final String TYPE = "type";
+    public static final String STORAGE_PATH = "storagePath";
+    public static final String CONTENT = "content";
+    public static final String TABLE = "table";
+    public static final String NAME = "name";
+    public static final String FIELD = "field";
+    public static final String OVERVIEW = "overview";
+    public static final String PROVIDER = "provider";
+    //registry path for store.json file at config registry
+    public static final String STORE_CONFIG_PATH = "/store/configs/store.json";
+
+
+}
